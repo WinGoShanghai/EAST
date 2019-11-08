27b8eb7f506cecb2a4119e6bdc33fd8b353eae55
BigWilky <35388143+BigWilky@users.noreply.github.com>
2019-11-08 12:59:39 +0800

    Update multigpu_train.py

--- multigpu_train.py
+++ multigpu_train.py
@@ -2,6 +2,12 @@ import time
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import slim
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 
 tf.app.flags.DEFINE_integer('input_size', 512, '')
 tf.app.flags.DEFINE_integer('batch_size_per_gpu', 14, '')
